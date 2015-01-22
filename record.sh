0f855550062d27f2b3eef7e265f5f0c28dcf074b
Matt Collier <collier.matthew@gmail.com>
2015-01-22 21:12:36 +0100

    cleanup

--- record.sh
+++ record.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-#IFS=$'\n'
-
-binfolder='/usr/local/bin/'
+source="${HOME}/settings.conf"
 
 # hash is 40 chars + 3 for underscore and two digit hour
 torrents=$(${binfolder}rtcontrol -qo hash,uploaded is_complete=yes \
   | awk '{printf "*3\r\n$3\r\nSET\r\n$43\r\n"$1"_"strftime("%H")"\r\n$"length($2)"\r\n"$2"\r\n"}' \
   | ${binfolder}redis-cli --pipe)
 
-#	> torrents.dat)
-#  | ${binfolder}redis-cli --pipe)
 
-#echo "$torrents"
-#${binfolder}redis-cli --pipe
