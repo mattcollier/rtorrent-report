988a209e7abbb9f272bba943f8a37f59a9915724
Matt Collier <collier.matthew@gmail.com>
2015-01-22 20:59:36 +0100

    externalize config

--- topten.sh
+++ topten.sh
@@ -1,8 +1,11 @@
 #/bin/bash
+
+source "${HOME}/settings.conf"
+
 for hour in $(seq 0 23); do
   echo "Top 10 for Hour: ${hour}"
   column=$(( hour + 3 ))
-  sort -nrk${column},${column} --field-separator=$'\t' data/twentyfour.dat | head -10 | awk 'BEGIN { FS = "\t" };{print $'${column}'"\t" $2}'
+  sort -nrk${column},${column} --field-separator=$'\t' "${datafolder}${datafile}" | head -10 | awk 'BEGIN { FS = "\t" };{print $'${column}'"\t" $2}'
   echo
 done
 
